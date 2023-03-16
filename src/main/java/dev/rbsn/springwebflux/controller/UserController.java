db6190ca9843ac0e7b307b8554f67975682e50cb
Robson S Fernandes <rbsn.dev@gmail.com>
2023-03-16 14:18:46 -0300

    create UserController interface

---
+++ src/main/java/dev/rbsn/springwebflux/controller/UserController.java
@@ -0,0 +1,30 @@
+package dev.rbsn.springwebflux.controller;
+
+import org.springframework.data.mongodb.repository.DeleteQuery;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public interface UserController {
+	
+	@PostMapping
+	ResponseEntity<Mono<Void>> save(@RequestBody UserRequest request);
+	
+	@GetMapping(value = "/{id}")
+	ResponseEntity<Mono<UserResponse>> find(@PathVariable String id);
+	
+	@GetMapping
+	ResponseEntity<Flux<UserResponse>> findAll();
+	
+	@PatchMapping(value = "/{id}")
+	ResponseEntity<Mono<UserResponse>> update(@PathVariable String id, @RequestBody UserRequest request);
+	
+	@DeleteQuery(value = "/{id}")
+	ResponseEntity<Mono<Void>> delete(@PathVariable String id);
+}
