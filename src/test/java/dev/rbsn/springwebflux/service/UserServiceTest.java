6c19ca430bb5602393ec63f74ce9888c6c568739
Robson S Fernandes <rbsn.dev@gmail.com>
2023-03-20 20:27:35 -0300

    add tdd to update user service

--- src/test/java/dev/rbsn/springwebflux/service/UserServiceTest.java
+++ src/test/java/dev/rbsn/springwebflux/service/UserServiceTest.java
@@ -75,4 +75,23 @@ class UserServiceTest {
 
         verify(repository, times(1)).findAll();
     }
+
+    @Test
+    void testUpdate() {
+        UserRequest request = new UserRequest(
+                "valid name", "valid@email.mail", "validPassword");
+        User entity = User.builder().build();
+
+        when(mapper.toEntity(any(UserRequest.class), any(User.class))).thenReturn(entity);
+        when(repository.findById(anyString())).thenReturn(Mono.just(entity));
+        when(repository.save(any(User.class))).thenReturn(Mono.just(entity));
+
+        Mono<User> result = service.update("validId", request);
+        StepVerifier.create(result)
+                .expectNextMatches(user -> user.getClass() == User.class)
+                .expectComplete()
+                .verify();
+
+        verify(repository, times(1)).save(any(User.class));
+    }
 }
